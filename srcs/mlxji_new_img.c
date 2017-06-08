a07e361c5b6e94d7189b6e37d72e561a6805517a
Jino <ntoniolo@student.42.fr>
2017-06-08 02:06:24 +0200

    protec malloc

--- srcs/mlxji_new_img.c
+++ srcs/mlxji_new_img.c
@@ -6,7 +6,7 @@
 /*   By: ntoniolo <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/28 12:35:28 by ntoniolo          #+#    #+#             */
-/*   Updated: 2016/11/29 12:11:01 by ntoniolo         ###   ########.fr       */
+/*   Updated: 2017/06/08 02:06:04 by ntoniolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ t_img	*mlxji_new_img(void *mlx, int width, int height)
 {
 	t_img	*img;
 
-	img = (t_img*)malloc(sizeof(t_img));
+	if (!(img = (t_img*)ft_memalloc(sizeof(t_img))))
+		exit(0);
 	if (img == NULL)
 		return (NULL);
 	img->img = mlx_new_image(mlx, width, height);
